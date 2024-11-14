ae1cd9905d9fc3daf93b8204d48873e8783e73a3
Kris Wilk <kris@reefnet.ca>
2024-11-13 22:54:57 -0500

    Create flippah.last-visit-separator.user.js

---
+++ flippah.last-visit-separator.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Flippah - Emphasize the "Last Visit" Separator
+// @description  Increases the thickness of the "last visit" separator to improve visibility, especially while scrolling.
+// @author       Kris Wilk
+// @version      1.0
+//
+// @namespace    https://github.com/kriswilk/userscripts
+// @homepage     https://github.com/kriswilk/userscripts
+// @supportURL   https://github.com/kriswilk/userscripts/issues
+// @downloadURL  https://kriswilk.github.io/userscripts/flippah.last-visit-separator.user.js
+//
+// @match        https://flippah.net/notifications/history*
+//
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=flippah.net
+//
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+  'use strict';
+  GM_addStyle('.history-last-visit:before, .history-last-visit:after {border-bottom: 20px solid red;}');
+})();
